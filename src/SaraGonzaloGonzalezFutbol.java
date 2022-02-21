3bcd631d8ed321dd84508675d7a63da5c90e3fad
sarag <sarag@IDEAPAD>
2022-02-21 07:09:43 +0100

    Primer commit

---
+++ src/SaraGonzaloGonzalezFutbol.java
@@ -0,0 +1,16 @@
+
+public class SaraGonzaloGonzalezFutbol {
+	private String aficion;
+	private int numGoles;
+	private String equipo;
+	
+	public SaraGonzaloGonzalezFutbol(String aficion,int numGoles,String equipo) {
+		this.aficion=aficion;
+		this.numGoles=numGoles;
+		this.equipo=equipo;
+	}
+	
+	public String getAficion() {
+		return(this.aficion);
+	}
+}
